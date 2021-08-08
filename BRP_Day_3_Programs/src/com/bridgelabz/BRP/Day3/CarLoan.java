ab49523c40c7efaeb25f4cf39960f57713d268af
pampapathi73 <pampapathik32@gmail.com>
2021-08-08 20:38:05 +0530

    [pampapathi] Day 3 Programs

---
+++ BRP_Day_3_Programs/src/com/bridgelabz/BRP/Day3/CarLoan.java
@@ -0,0 +1,22 @@
+package com.bridgelabz.BRP.Day3;
+
+public class CarLoan {
+
+	public static void main(String[] args ) {
+		// TODO Auto-generated method stub
+		
+		    double principal = Double.parseDouble(args[0]);
+	        double years = Double.parseDouble(args[1]);
+	        double rate = Double.parseDouble(args[2]);
+
+	        double r = (rate / 100) / 12;   // monthly interest rate
+	        double n = 12 * years;          // number of months
+
+            double payment  = (principal * r) / (1 - Math.pow(1+r, -n));
+	        double interest = payment * n - principal;
+
+	        System.out.println("Monthly payments = " + payment);
+	        System.out.println("Total interest   = " + interest);
+    }
+
+}
